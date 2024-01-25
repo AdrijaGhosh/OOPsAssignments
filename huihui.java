23454ea3a088024621c2a52b65bb8a43a5023c6f
AdrijaGhosh <121801620+AdrijaGhosh@users.noreply.github.com>
2024-01-25 11:40:43 +0530

    Add files via upload

---
+++ huihui.java
@@ -0,0 +1,30 @@
+public class huihui
+{
+	public static void main(String args[])
+	{
+		int score=49;
+		switch(score)
+		{
+		 	case 90:
+		 	{
+		 	 	System.out.println("Very good");
+		 	 	break;
+		 	}
+		 	case 70:
+		 	{
+		 	 	System.out.println("good");
+		 	 	break;
+		 	}
+		 	case 50:
+		 	{
+		 	 	System.out.println("OK");
+		 	 	break;
+		 	}
+		 	default:
+		 	{
+		 		System.out.println("Value not found");
+		 	}
+		 	
+		}
+	}
+}
