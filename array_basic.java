0ba8df7bc155da43d29e12e7d1e31af093c16197
AdrijaGhosh <121801620+AdrijaGhosh@users.noreply.github.com>
2024-01-25 12:06:11 +0530

    Add files via upload

---
+++ array_basic.java
@@ -0,0 +1,14 @@
+public class array_basic
+{
+	public static void main(String args[])
+	{
+	int arr[]={10,20,30,40,50,60,70};
+	int x[]={1,2,3};
+	int i;
+	for(i=0;i<7;i++)
+	System.out.println("arr["+i+"] :"+arr[i]);
+	for(i=0;i<3;i++)
+	System.out.println("x["+i+"] :"+x[i]);
+	}
+}
+	
